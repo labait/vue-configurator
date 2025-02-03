3ac4fbf46219aaa2f16c138cd673c6cafeadfbf8
Antonio Molinari <molinari@incode.it>
2025-02-03 12:26:24 +0100

    enhance configurator controls with improved styling and active state indication

--- src/components/configurator.vue
+++ src/components/configurator.vue
@@ -64,14 +64,17 @@
       <ConfiguratorLayer name="case" />    
     </div>
     <div class="controls">
-      <div class="control case">
-        <button class="prev" @click="setLayer('case', (configuration.case - 1) < 1 ? configurations.length : configuration.case - 1)">Prev</button>
-        <button class="next" @click="setLayer('case', (configuration.case + 1) > configurations.length ? 1 : configuration.case + 1)">Next</button>
+      <div class="control case m-1">
+        <label for="case" class="px-2">Case</label>
+        <button class="btn btn-light prev mx-1" @click="setLayer('case', (configuration.case - 1) < 1 ? configurations.length : configuration.case - 1)">Prev</button>
+        <button class="btn btn-light next mx-1" @click="setLayer('case', (configuration.case + 1) > configurations.length ? 1 : configuration.case + 1)">Next</button>
+        <div class="info px-2 d-inline">{{ configuration.case }}</div>
       </div>
-      <div class="control ring">
-        <button @click="setLayer('ring', 1)">ring1</button>
-        <button @click="setLayer('ring', 2)">ring2</button>
-        <button @click="setLayer('ring', 3)">ring3</button>
+      <div class="control ring m-1">
+        <label for="ring" class="px-2">Ring</label>
+        <button class="btn mx-1" :class="configuration.ring === 1 ? 'btn-primary' : 'btn-light'" @click="setLayer('ring', 1)">ring1</button>
+        <button class="btn mx-1" :class="configuration.ring === 2 ? 'btn-primary' : 'btn-light'" @click="setLayer('ring', 2)">ring2</button>
+        <button class="btn mx-1" :class="configuration.ring === 3 ? 'btn-primary' : 'btn-light'" @click="setLayer('ring', 3)">ring3</button>
       </div>
     </div>
   </div>
@@ -109,6 +112,11 @@
       text-align: center;
       .control {
         text-align: center;
+      }
+      button {
+        &.active {
+          background-color: red;
+        }
       }
     }
   
